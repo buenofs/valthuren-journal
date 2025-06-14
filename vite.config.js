2d77c79ad8d127b70fca670642a2f272b9895493
Felipe Bueno <bueeno123@gmail.com>
2025-06-14 15:12:38 -0300

    fix logo 2

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: '#1a1816',
         icons: [
           {
-            src: 'https://lyuoqsiipcstauszdazg.supabase.co/storage/v1/object/sign/ui/valthuren-logo-192.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2M1YTkzNWVjLTc2NDAtNDdmMy04OTQyLWU0ZDQ3MDY4NjQ0ZiJ9.eyJ1cmwiOiJ1aS92YWx0aHVyZW4tbG9nby0xOTIucG5nIiwiaWF0IjoxNzQ3NDg2NjI2LCJleHAiOjE3NzkwMjI2MjZ9.s9xhi0wZsO_nHwO8XwJRTvKDR0QmZ-eIrg6asl9_RmU',
+            src: 'https://lyuoqsiipcstauszdazg.supabase.co/storage/v1/object/public/ui//valthuren-logo-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'https://lyuoqsiipcstauszdazg.supabase.co/storage/v1/object/sign/ui/valthuren-logo-512.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2M1YTkzNWVjLTc2NDAtNDdmMy04OTQyLWU0ZDQ3MDY4NjQ0ZiJ9.eyJ1cmwiOiJ1aS92YWx0aHVyZW4tbG9nby01MTIucG5nIiwiaWF0IjoxNzQ3NDg2NjE1LCJleHAiOjE3NzkwMjI2MTV9.X-SdAwv8NUvnLzwL06VWq89AMurKBqQgxzHWVq6YRMI',
+            src: 'https://lyuoqsiipcstauszdazg.supabase.co/storage/v1/object/public/ui//valthuren-logo-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
